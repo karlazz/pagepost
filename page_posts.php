2cda88ec9e48142f8ccb193c5bfdd4f941e9b5ea
Karla Leibowitz <liminal.karla@gmail.com>
2012-01-13 13:37:26 -0800

    got rid of extraneous characters

--- page_posts.php
+++ page_posts.php
@@ -6,13 +6,12 @@
  */
 /*
 Plugin Name: PagePost
-Plugin URI: http://www.techliminal.com
+Plugin URI: http://www.karlakarla.com
 Description: This will put posts on a page that has its own text.  It is a shortcode pagepost with num=,cat=,order=,orderby= for arguments
 Author: Karla Leibowitz
 Version: 1
 Author URI: http://karlakarla.com
 */
-
 function get_content_with_formatting ($more_link_text = '(more...)', $stripteaser = 0, $more_file = '') {
         global $more; $more=0;
 	$content = get_the_content($more_link_text, $stripteaser, $more_file);
@@ -20,7 +19,6 @@ function get_content_with_formatting ($more_link_text = '(more...)', $striptease
 	$content = str_replace(']]>', ']]&gt;', $content);
 	return $content;
 }
-
 function pp_list($atts, $content = null) {
         extract(shortcode_atts(array(
                 "num" => '1',
